88e50a0dfb231469a8dc1f96aaaba0a7a826b0ac
ethanharianto <ethanhanshr919@gmail.com>
2023-06-28 22:38:06 -0400

    task 3

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -27,6 +27,9 @@ class Graph extends Component<IProps, {}> {
       top_ask_price: 'float',
       top_bid_price: 'float',
       timestamp: 'date',
+      upper_bound: 'float',
+      lower_bound: 'float',
+      trigger_alert: 'float',
     };
 
     if (window.perspective && window.perspective.worker()) {
